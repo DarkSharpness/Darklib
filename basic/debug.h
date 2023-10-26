97a5cc4743a652d1f7e27cd0e3d1691c64c3798f
DarkSharpness_XzY <2040703891@qq.com>
2023-10-26 18:26:50 +0800

    Upd(debug): Add more debug macros.

--- basic/debug.h
+++ basic/debug.h
@@ -1,28 +1,32 @@
 #pragma once
 
 #include <iostream>
+#include <format>
 
 namespace dark {
 
 
 struct error {
     std::string data;
-    explicit error(error *) {}
-
     explicit error(std::string __s) : data(std::move(__s))
     { std::cerr << "\n\033[31mFatal error: " << data << "\n\033[0m"; }
-
     const char *what() const noexcept { return data.c_str(); }
 };
 
 
 struct warning {
     std::string data;
-    explicit warning(warning *) {}
-
-    warning(std::string __s) {
+    explicit warning(std::string __s) {
         std::cerr << "\033[33mWarning: " << __s << "\n\033[0m";
     }
+    const char *what() const noexcept { return data.c_str(); }
+};
+
+
+struct normal {
+    std::string data;
+    explicit normal(std::string __s) : data(std::move(__s))
+    { std::cerr << "\033[32m" << data << "\n\033[0m"; }
 
     const char *what() const noexcept { return data.c_str(); }
 };
