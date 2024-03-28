1e8832c9e526e786aa1b7a9b57dee7c976249fa3
DarkSharpness_XzY <2040703891@qq.com>
2024-03-28 15:30:33 +0800

    Upd(debug): Complete debug header. Feature: - panic, panic_handler - error/warning/normal

--- utility/basic.h
+++ utility/basic.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "debug.h"
 #include <version>
 #include <type_traits>
 
@@ -7,5 +8,8 @@ namespace dark {
 using size_t    = std::size_t;
 using ssize_t   = std::make_signed_t<size_t>;
 using ptrdiff_t = std::ptrdiff_t;
+
+/* An unreachable function. */
+[[noreturn]] inline void unreachable() { __builtin_unreachable(); }
 
 } // namespace dark
---
+++ utility/debug.h
@@ -0,0 +1,77 @@
+#pragma once
+#ifdef _DEBUG
+#include "console.h"
+#include <source_location>
+
+namespace dark::debug {
+
+namespace __detail::__debug {
+
+template <console::Color _Color>
+struct printer : std::exception {
+    std::string message;
+    explicit printer(std::string __msg)
+    noexcept : message(std::move(__msg)) {}
+    const char *what() const noexcept override { return message.c_str(); }
+    const void print() const { return console::print(message, _Color); }
+};
+
+} // namespace __detail::__debug
+
+using error     = __detail::__debug::printer <console::Color::RED>;
+using warning   = __detail::__debug::printer <console::Color::YELLOW>;
+using normal    = __detail::__debug::printer <console::Color::GREEN>;
+
+[[noreturn]] inline void panic_handler(
+    std::string_view __msg, std::source_location __loc) {
+    error { std::format("Panic at {}:{}:{}: {}",
+        __loc.file_name(),
+        __loc.line(),
+        __loc.column(), __msg) }.print();
+    std::terminate();
+}
+
+} // namespace dark::debug
+
+#ifndef panic_handler
+/**
+ * @brief Custom panic handler.
+ * It will print out where the panic happened.
+ */
+#define panic_handler(x,y) do { ::dark::debug::panic_handler(x,y); } while (0)
+#endif // panic_handler
+
+#ifdef _RELEASE
+#undef panic_handler
+// In release mode, we can still use the debug library,
+// while the panic handler will be turned off.
+#define panic_handler(x,y) __builtin_unreachable()
+#endif // _RELEASE
+
+#endif // _DEBUG
+
+#ifndef panic_handler
+// Define the default panic handler.
+#define panic_handler(x,y) __builtin_unreachable()
+#endif // panic_handler
+
+#ifndef panic
+/**
+ * @brief Define the panic macro.
+ * @note By default, this macro will call the panic_handler macro.
+ * 
+ * If user does not define the panic_handler, the panic macro
+ * will expand to __builtin_unreachable() in release mode.
+ * It will turn to default panic_handler in debug mode.
+ * 
+ * If you just want to use the feature of debug library,
+ * but don't want to use the debug-mode panic_handler, you have 2 options:
+ * 
+ * 1. Define your own panic_handler, place it in the top of your code.
+ * 2. Define _RELEASE macro along with _DEBUG macro. Panic will still
+ *  expand to __builtin_unreachable() in this mode.
+ * 
+ * @attention This function will never return!
+ */
+#define panic(x) panic_handler(x, std::source_location::current())
+#endif // panic
